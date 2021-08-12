3f5ee7ed815a1cb087f9e1f7017709a1ef1e190a
emanuelpras <emanueldina@refactory.id>
2021-08-12 18:12:01 +0700

    add aws lambda support

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module middleware-mmksi
 go 1.15
 
 require (
+	github.com/apex/gateway v1.1.2
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.3
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
--- main.go
+++ main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"log"
+	"os"
 
 	mrpControllers "middleware-mmksi/dsf/mrp/controller"
 	jwtControllers "middleware-mmksi/jwt/controller"
 	mmksiControllers "middleware-mmksi/mmksi/controller"
 	"middleware-mmksi/util"
 
+	"github.com/apex/gateway"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	RunLocally()
+	// RunInAWS()
+}
+
+func RunLocally() {
 	err := godotenv.Load()
 
 	var (
@@ -41,4 +48,42 @@ func main() {
 	r.POST("/mmksi/vehicle", authController.Auth, tokenController.GetToken, mmksiController.GetVehicleColor)
 
 	r.Run()
+}
+
+func RunInAWS() {
+	addr := ":" + os.Getenv("PORT")
+	log.Fatal(gateway.ListenAndServe(addr, routerEngine()))
+}
+
+func routerEngine() *gin.Engine {
+	// set server mode
+	gin.SetMode(gin.DebugMode)
+
+	r := gin.New()
+
+	// Global middleware
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	//=====================
+	var (
+		authController  jwtControllers.JwtController     = jwtControllers.NewJwtController(util.ProvideAuthService())
+		mrpController   mrpControllers.MrpController     = mrpControllers.NewMrpController(util.ProvideMrpService())
+		tokenController mmksiControllers.MmksiController = mmksiControllers.NewMmksiController(util.ProvideTokenService())
+		mmksiController mmksiControllers.MmksiController = mmksiControllers.NewMmksiController(util.ProvideMmksiService())
+	)
+
+	// token route
+	r.POST("/auth/token", authController.GetFirstToken)
+
+	// dsf route
+	r.GET("/dsf/tradein/vehicles", authController.Auth, mrpController.GetVehicles)
+	r.GET("/dsf/tradein/regions", authController.Auth, mrpController.GetRegions)
+	r.POST("/dsf/tradein/prediction", authController.Auth, mrpController.GetPrediction)
+
+	// mmksi route
+	r.POST("/mmksi/getData", authController.Auth, tokenController.GetToken, mmksiController.GetVehicle)
+	r.POST("/mmksi/vehicle", authController.Auth, tokenController.GetToken, mmksiController.GetVehicleColor)
+
+	return r
 }
